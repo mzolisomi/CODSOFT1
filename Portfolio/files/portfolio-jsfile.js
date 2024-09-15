e513374ab341b6958e85a88bb75f6d01c5e5d724
Mzoli Somi <mzolisomie18@gmail.com>
2024-09-15 17:00:15 +0200

    push

--- Portfolio/files/portfolio-jsfile.js
+++ Portfolio/files/portfolio-jsfile.js
@@ -2,7 +2,12 @@
  * @Author: Your name
  * @Date:   2024-09-12 13:38:04
  * @Last Modified by:   Your name
- * @Last Modified time: 2024-09-15 14:25:46
+ * @Last Modified time: 2024-09-15 15:03:24
  */
 'use strict';
 clock = document.getElementsByClassName('clock-project');
+// https://www.linkedin.com/in/mzoli-somi
+
+function openLinkedIn () {
+  document.createElement('form').URL('https://www.linkedin.com/in/mzoli-somi');
+}
