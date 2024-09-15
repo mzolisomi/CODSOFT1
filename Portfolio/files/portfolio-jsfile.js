67ece434f223afc88837b54044af5b974dc76c65
Mzoli Somi <mzolisomie18@gmail.com>
2024-09-15 14:34:29 +0200

    push

---
+++ Portfolio/files/portfolio-jsfile.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Your name
+ * @Date:   2024-09-12 13:38:04
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2024-09-15 14:25:46
+ */
+'use strict';
+clock = document.getElementsByClassName('clock-project');
